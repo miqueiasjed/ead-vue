e3d470880b0a9d6bf8208960db41030ce900da16
Miquéias <miqueias_jed@hotmail.com>
2022-09-01 12:24:32 -0300

    edit vue student

--- src/views/Students/EditStudents.vue
+++ src/views/Students/EditStudents.vue
@@ -1,17 +1,49 @@
 <template>
-    <div>
-        <h1>Editar Aluno</h1>
-
-        <form action="#" method="post" @submit.prevent="editStudent">
-            <input type="text" name="name" placeholder="Nome" v-model="student.name">
-            <input type="text" name="email" placeholder="Email" v-model="student.email">
-            <input type="date" name="birth_date" placeholder="Data de Nascimento" v-model="student.birth_date">
-            <button type="submit" :disabled="student.loading">
-                <span v-if="student.loading">Aguarde...</span>
-                <span v-else>Enviar</span>
-            </button>
-        </form>
+    <div class="mt-10 sm:mt-0">
+    <div class="flex-1 min-w-0 pb-4">
+        <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">Edição de Aluno</h1>
     </div>
+    
+    <div class="md:grid md:grid-cols-2 md:gap-6">
+    <div class="mt-5 md:mt-0 md:col-span-2">
+    <form @submit.prevent='editStudent' method="POST">
+        <div class="overflow-hidden shadow sm:rounded-md">
+        <div class="px-4 py-5 bg-white sm:p-6">
+            <div class="grid grid-cols-6 gap-6">
+
+            <div class="col-span-4 sm:col-span-3">
+                <label for="name" class="block text-sm font-medium text-gray-700">Nome</label>
+                <input v-model="student.name" type="text" name="name" id="name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            
+                <!-- <div v-if="student.errors.obs" v-text="student.errors.obs" class="mt-1 text-xs text-red-500"></div> -->
+            </div>
+
+            <div class="col-span-4 sm:col-span-3">
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input v-model="student.email" type="email" name="email" id="email" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            
+                <!-- <div v-if="student.errors.obs" v-text="student.errors.obs" class="mt-1 text-xs text-red-500"></div> -->
+            </div>
+
+            <div class="col-span-4 sm:col-span-3">
+                <label for="birth_date" class="block text-sm font-medium text-gray-700">Data de Nascimento</label>
+                <input v-model="student.birth_date" type="date" name="birth_date" id="birth_date" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            
+                <!-- <div v-if="student.errors.birth_date" v-text="student.errors.birth_date" class="mt-1 text-xs text-red-500"></div> -->
+            </div>
+            
+            </div>
+            <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
+                <router-link :to="{name: 'students.index'}" class="text-indigo-600 hover:text-indigo-900">Voltar</router-link>
+                <button type="submit" class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Salvar</button>
+            </div>
+              
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,24 +61,22 @@ export default {
         const student = reactive({
             name: '',
             email: '',
-            loading: false
+            birth_date: '',
+            course_id: ''
         })
         onMounted(async () => {
-            student.loading = true
             StudentService.getStudent(props.id)
                         .then(response => {
                             const studentR = response.data.data
+                            student.course_id = studentR.course_id
                             student.name = studentR.name
                             student.email = studentR.email
                             student.birth_date = studentR.birth_date
                         })
-                        .finally(() => student.loading = false)
         })
         const editStudent = () => {
-            student.loading = true
             StudentService.editStudent(props.id, {...student})
                         .then(() => router.push({name: 'students.index'}))
-                        .finally(() => student.loading = false)
         }
         return {
             editStudent,
